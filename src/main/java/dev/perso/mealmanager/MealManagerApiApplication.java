a045b2f6e97fde66514e76c77632637483ec3914
chevaliergael <doggyexistence@yahoo.fr>
2022-07-07 11:58:28 +0200

    first commit

---
+++ src/main/java/dev/perso/mealmanager/MealManagerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.perso.mealmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/perso/mealmanager/MealManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.perso.mealmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
